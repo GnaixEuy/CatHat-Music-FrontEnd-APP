6d9acf9b766c44648582785ad25c21e000aafac5
GnaixEuy <sgghcj@live.com>
2022-02-09 21:44:48 +0800

    :bento: 添加了推荐音乐人组件

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,6 +2,8 @@ import 'package:cathat_music_app/pages/home/header.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'musician_banner.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -11,6 +13,10 @@ class HomePage extends StatelessWidget {
         body: Column(
       children: const <Widget>[
         Header(),
+        SizedBox(height: 40),
+        MusicianBanner(
+          title: "热榜音乐人",
+        )
       ],
     ));
   }
